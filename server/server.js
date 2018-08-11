dfdba46d79f4180817b246bd068861945d3f31e7
yusufyilmz <yusufyilmz@yusufs-MacBook-Pro.local>
2018-08-11 12:52:33 +0300

    added get list

--- server/server.js
+++ server/server.js
@@ -20,10 +20,17 @@ app.post('/todos', (req, res) => {
     })
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status.send(err);
+    })
+});
 
 app.listen(3000, () =>{
     console.log('Started on port 3000');
 });
 
 module.exports = { app };
-    
+
