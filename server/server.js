69bec7423a4a0a3bc9d1bf96bb4f2423209cedbc
yusufyilmz <yusufyilmz@yusufs-MacBook-Pro.local>
2018-08-11 14:23:53 +0300

    added delete

--- server/server.js
+++ server/server.js
@@ -57,10 +57,24 @@ app.get('/todos/:id', (req, res) => {
     }).catch((err) => {
         res.status(400).send();
     })
-
-
 });
 
+app.delete('/todos/:id', (req, res) => {
+
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(req.params.id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({
+            todo
+        });
+    }).catch((err) => {
+        res.status(400).send();
+    })
+});
 
 app.listen(port, () => {
     console.log('Started on port', port);
