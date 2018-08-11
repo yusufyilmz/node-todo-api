565787c9da07193cbc82eda9912a44664c553f5a
yusufyilmz <yusufyilmz@yusufs-MacBook-Pro.local>
2018-08-11 13:30:42 +0300

    added get by queries

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {
+    ObjectID
+} = require('mongodb');
 
-var { mongoose} = require('./db/mongoose');
-var { Todo } = require('./models/todo');
-var { User } = require('./models/user');
+var {
+    mongoose
+} = require('./db/mongoose');
+var {
+    Todo
+} = require('./models/todo');
+var {
+    User
+} = require('./models/user');
 
 var app = express();
 app.use(bodyParser.json());
@@ -11,7 +20,7 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
-        text : req.body.text,
+        text: req.body.text,
     });
     todo.save().then((doc) => {
         res.send(doc);
@@ -22,15 +31,40 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({
+            todos
+        });
     }, (err) => {
-        res.status.send(err);
+        res.status(400).send(err);
     })
 });
 
-app.listen(3000, () =>{
+app.get('/todos/:id', (req, res) => {
+
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(404).send();
+    }
+    Todo.findById({
+        _id: req.params.id
+    }).then((todo) => {
+        if (!todo) {
+            return res.status(404);
+        }
+
+        res.send({todo});
+
+    }).catch((err) => {
+        res.status(400).send();
+    })
+
+
+});
+
+
+app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
-module.exports = { app };
-
+module.exports = {
+    app
+};
